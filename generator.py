86504cbb31faf2fab0b32bc5bb0e3ae4eb96e484
oickle <oycell@aol.com>
2024-05-04 19:26:17 -0400

    Created basic player

--- generator.py
+++ generator.py
@@ -1,9 +1,22 @@
 import vlc
+from pytube import YouTube
+from time import sleep
 
-url = "https://www.youtube.com/watch?v=AE005nZeF-A"
-instance = vlc.Instance()
-player = instance.media_player_new()
-media = instance.media_new(url)
-media.get_mrl()
-player.set_media(media)
-player.play()
+url = "https://www.youtube.com/watch?v=1RKqOmSkGgM"
+
+# Downloads audio from url to queue
+youtube = YouTube(url)
+audio = youtube.streams.filter(only_audio=True).first()
+song_count = 1
+audio.download("queue", "song_" + str(song_count) + ".mp3")
+
+# Plays first song in queue
+p = vlc.MediaPlayer(f'queue/song_1.mp3')
+p.play()
+
+# Gives vlc player time to open
+sleep(5)
+
+# Prevents closing as long as vlc player is running
+while p.is_playing():
+    sleep(1)
