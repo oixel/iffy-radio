dd05b93127c2c73878aa29054de631f85a58669b
oickle <oycell@aol.com>
2024-05-04 20:56:51 -0400

    Create test play function

--- player.py
+++ player.py
@@ -1,24 +1,32 @@
 import vlc
-from pytube import YouTube
+import pytube
 from time import sleep
 
 # Test link
-url = "https://www.youtube.com/watch?v=1RKqOmSkGgM"
+# url = "https://www.youtube.com/watch?v=1RKqOmSkGgM"
 
-# Downloads audio from url to queue
-youtube = YouTube(url)
-audio = youtube.streams.filter(only_audio=True).first()
+# # Downloads audio from url to queue
+# youtube = pytube.YouTube(url)
+# audio = youtube.streams.filter(only_audio=True).first()
 
-song_count = 1
-audio.download("queue", "song_" + str(song_count) + ".mp3")
+# song_count = 1
+# audio.download("queue", "song_" + str(song_count) + ".mp3")
 
-# Plays first song in queue
-p = vlc.MediaPlayer(f'queue/song_1.mp3')
-p.play()
+# # Plays first song in queue
+# p = vlc.MediaPlayer(f'queue/song_1.mp3')
+# p.play()
 
-# Gives vlc player time to open
-sleep(5)
+# # Gives vlc player time to open
+# sleep(5)
 
-# Prevents closing as long as vlc player is running
-while p.is_playing():
-    sleep(1)
+# # Prevents closing as long as vlc player is running
+# while p.is_playing():
+#     sleep(1)
+
+#
+def play(command):
+    if command == None:
+        print("No QR Code Provided")
+        return
+
+    print(command + "should be played")
