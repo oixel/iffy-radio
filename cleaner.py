10543fd80183071bb2f7a8977f539f720554b53f
oickle <oycell@aol.com>
2024-05-05 02:08:40 -0400

    Check when script is run

--- cleaner.py
+++ cleaner.py
@@ -10,34 +10,35 @@ def clear_at_folder(path):
     for file in files:
         os.remove(file)
 
-# Loops until an allowed choice is made (1, 2, or 3)
-while True:
+if __name__ == "__main__":
+    # Loops until an allowed choice is made (1, 2, or 3)
+    while True:
+        print("------------------------------------------")
+        print("What type of code is being generated?")
+        print("1. Queue")
+        print("2. QR Codes")
+        print("3. All")
+
+        try:
+            choiceInt = int(input('Choice: '))
+
+            if choiceInt < 1 or choiceInt > 3:
+                raise
+            
+            choice = choiceInt
+
+            break
+        except:
+            print("\nERROR: Invalid choice, please try again.")
+            continue
+
     print("------------------------------------------")
-    print("What type of code is being generated?")
-    print("1. Queue")
-    print("2. QR Codes")
-    print("3. All")
 
-    try:
-        choiceInt = int(input('Choice: '))
-
-        if choiceInt < 1 or choiceInt > 3:
-            raise
-        
-        choice = choiceInt
-
-        break
-    except:
-        print("\nERROR: Invalid choice, please try again.")
-        continue
-
-print("------------------------------------------")
-
-# Deletes files in folders depending on choice input
-if choice == 1:
-    clear_at_folder("queue") 
-elif choice == 2:
-    clear_at_folder("qrcodes")
-else:
-    clear_at_folder("queue")
-    clear_at_folder("qrcodes")
+    # Deletes files in folders depending on choice input
+    if choice == 1:
+        clear_at_folder("queue") 
+    elif choice == 2:
+        clear_at_folder("qrcodes")
+    else:
+        clear_at_folder("queue")
+        clear_at_folder("qrcodes")
