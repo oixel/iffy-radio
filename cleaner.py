f851cf886ca379d3dc7516d557f6145f71f58bd4
oickle <oycell@aol.com>
2024-05-05 01:17:27 -0400

    Add comments

--- cleaner.py
+++ cleaner.py
@@ -1,13 +1,16 @@
 import os
 import glob
 
+# Stores user's choice for what to delete
 choice = ""
 
+# Deletes all files in given folder
 def clear_at_folder(path):
     files = glob.glob(f"{path}/*")
     for file in files:
         os.remove(file)
 
+# Loops until an allowed choice is made (1, 2, or 3)
 while True:
     print("------------------------------------------")
     print("What type of code is being generated?")
@@ -28,7 +31,9 @@ while True:
         print("\nERROR: Invalid choice, please try again.")
         continue
 
-# 
+print("------------------------------------------")
+
+# Deletes files in folders depending on choice input
 if choice == 1:
     clear_at_folder("queue")
     
