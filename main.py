daff8861bed6a4d2c76407b7ec0f866dc9286485
oickle <oycell@aol.com>
2024-06-19 10:25:09 -0400

    Test now finds all metadata, not just album cover

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import album_grabber as AG
+import data_grabber as DG
 from pytube import *
 import webbrowser
 
 TEST_PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PL2fTbjKYTzKcb4w0rhNC76L-MER585BJa'
 
 playlist = Playlist(TEST_PLAYLIST_URL)
-num = 1
+
 for song_url in playlist.video_urls:
-    webbrowser.open(AG.get_album_cover(song_url), '\n')
+    dg = DG.DataGrabber(song_url)
+    print(dg.get_data())
+
+#TEST_SONG = YouTube()
