de177b8826d1f32cda01b3c4919a8d92f88ae891
oickle <oycell@aol.com>
2024-06-19 00:48:43 -0400

    Implement basic webscraping for image

--- album_grabber.py
+++ album_grabber.py
@@ -2,16 +2,22 @@ import urllib
 import urllib.request
 from bs4 import BeautifulSoup as BS
 
-print("START\n\n\n")
 html = urllib.request.urlopen("https://www.youtube.com/watch?v=ws5K_5G_xvI").read()
 
-soup = BS(html)
+bs = BS(html, "html.parser")
 
-html = soup.prettify()  #bs is your BeautifulSoup object
+html = bs.prettify()
 
-with open("out.txt","w") as out:
-    for i in range(0, len(html)):
-        try:
-            out.write(html[i])
-        except Exception:
-            1+1
+MARKER = '{"image":{"sources":[{"url":"'
+
+if MARKER in html:
+    start = html.find(MARKER) + len(MARKER)
+    end = start
+    for i in range(start, len(html)):
+        if html[i] == '"':
+            end = i
+            break
+    src = html[start:end]
+    print(src)
+else:
+    print("No album art available, grabbing thumbnail instead")
