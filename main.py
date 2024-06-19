f4f82d48cfc449912a798ae9ddf742a3c706bf3e
oickle <oycell@aol.com>
2024-06-19 01:34:13 -0400

    Test code now automatically opens album cover

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import album_grabber as AG
 from pytube import *
+import webbrowser
 
 TEST_PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PL2fTbjKYTzKcb4w0rhNC76L-MER585BJa'
 
 playlist = Playlist(TEST_PLAYLIST_URL)
 num = 1
 for song_url in playlist.video_urls:
-    print(AG.get_album_cover(song_url), '\n')
+    webbrowser.open(AG.get_album_cover(song_url), '\n')
