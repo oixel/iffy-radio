e1fb2359a95069e20ece8c74a15fe7f33a48221f
oickle <oycell@aol.com>
2024-06-19 16:19:23 -0400

    Add range and change test video

--- scrapped_mm/html_write.py
+++ scrapped_mm/html_write.py
@@ -3,11 +3,11 @@ import urllib.request
 from bs4 import BeautifulSoup as BS
 
 print("START\n\n\n")
-html = urllib.request.urlopen("https://www.youtube.com/watch?v=B9synWjqBn8").read()
+html = urllib.request.urlopen("https://www.youtube.com/watch?v=bIX_ouNJsTs&list=PL2fTbjKYTzKcb4w0rhNC76L-MER585BJa&index=9").read()
 
 soup = BS(html, "html.parser")
 
-html = soup.prettify()  #bs is your BeautifulSoup object
+html = soup.prettify()[850000:]  #bs is your BeautifulSoup object
 
 with open("out.txt","w") as out:
     for i in range(0, len(html)):
