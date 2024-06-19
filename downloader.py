08f2ce63ea367d3df6e5a505d1ea2bd99b61272f
oickle <oycell@aol.com>
2024-06-19 16:45:16 -0400

    Implement song downloading

--- downloader.py
+++ downloader.py
@@ -1,6 +1,19 @@
 import requests
+from pytube import *
 
 # Takes in image source URL and downloads it at parameterized path
-def download_image(url, path) -> None:
+def download_cover(url, song_name) -> None:
     image_data = requests.get(url).content
+
+    # Downloads album cover art in format of SongName.jpg in album_covers folder
+    path =  f'content/album_covers/{song_name}.jpg'
     open(path, "wb").write(image_data)
+
+# Downloads song at given url
+def download_song(url, song_name):
+    # Downloads audio from url to queue folder
+    youtube = YouTube(url)
+    audio = youtube.streams.filter(only_audio=True).first()
+
+    # Stores song in format of SongName.mp3 in songs folder
+    audio.download('content/songs/', f'{song_name}.mp3')
