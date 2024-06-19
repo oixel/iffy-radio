0e439ded49169b2109c103be745f58f617e4245d
oickle <oycell@aol.com>
2024-06-19 11:35:42 -0400

    Fix weird boilerplate grabbing bug, restructure code, add comments

--- data_grabber.py
+++ data_grabber.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as BS
 import album_grabber as AG
 
 class DataGrabber:
+    # Each marker represents the HTML that comes right before each specific piece of metadata
     COVER_MARKER = 'https://lh3'
     SONG_MARKER = '"imageStyle":"VIDEO_ATTRIBUTE_IMAGE_STYLE_SQUARE","title":"'
     ARTIST_MARKER = '","subtitle":"'
@@ -14,41 +15,47 @@ class DataGrabber:
 
         # Uses BeautifulSoup to parse through the HTML data and convert it to a "legible" string
         bs = BS(html, "html.parser")
-        self.html = bs.prettify()[800000:]
+        self.html = bs.prettify()[800000:]  # Everything before ~800000 does not include metadata info
 
         # Creates an empty dictionary to fill with metadata
         self.metadata = {}
 
-        self.start = 0
+        # Makes sure that first search for album cover starts at the beginning of HTML
+        self.prev_end = 0
     
-    # 
-    def find_data(self, marker, old_start, offset):
-        start = self.html.find(marker, old_start) + offset
-        end = self.html.find('"', start)
+    # Takes marker and searches for it from the previous data's end character index to the end of the HTML
+    def find_data(self, marker, use_offset=True):
+        # Only turn off offset of marker's character count if specifically stated in function call
+        offset = 0 if use_offset == False else len(marker)
+
+        # Start search at last data's end and add offset of character count of marker to make sure it searches past marker
+        start = self.html.find(marker, self.prev_end) + offset
+        end = self.html.find('"', start)  # " indicates the end of metadata value
         data = self.html[start:end]
         
-        new_start = end if start != -1 else old_start
-        
-        if not data or end < 250:
+        # When there is a bug or metadata does not exist, ensure that new_start does not change and data is type None
+        if self.prev_end > start or not data:
+            new_start = self.prev_end
             data = None
-
+        else:
+            # Otherwise, leave data as what it is found to be and updata the new_start to be at the end of current data
+            new_start = end
+    
+        # Returns the value of the new start and the metadata that was found (or not found)
         return new_start, data
 
     #
     def get_data(self):
         # Gets album cover's source
-        self.search_start, self.metadata["cover_src"] = self.find_data(self.COVER_MARKER, self.start, 0)
+        self.prev_end, self.metadata["cover_src"] = self.find_data(self.COVER_MARKER, False)
 
         # Gets song's name
-        offset = len(self.SONG_MARKER)
-        self.search_start, self.metadata["song"] = self.find_data(self.SONG_MARKER, self.start, offset)
+        self.prev_end, self.metadata["song"] = self.find_data(self.SONG_MARKER)
 
         # Gets artist's name
-        offset = len(self.ARTIST_MARKER)
-        self.search_start, self.metadata["artist"] = self.find_data(self.ARTIST_MARKER, self.start, offset)
+        self.prev_end, self.metadata["artist"] = self.find_data(self.ARTIST_MARKER)
 
         # Gets album's name
-        offset = len(self.ALBUM_MARKER)
-        self.search_start, self.metadata["album"] = self.find_data(self.ALBUM_MARKER, self.start, offset)
+        self.prev_end, self.metadata["album"] = self.find_data(self.ALBUM_MARKER)
 
         return self.metadata
