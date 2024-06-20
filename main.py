0e983e155ea50994dfd26af5911c67508f47ba6a
oickle <oycell@aol.com>
2024-06-19 21:29:52 -0400

    Implement metadata writing!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import data_grabber as DG
 from downloader import *
 from pytube import *
-import webbrowser
+import taglib
 
 TEST_PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PL2fTbjKYTzKcb4w0rhNC76L-MER585BJa'
 
@@ -13,7 +13,12 @@ for song_url in playlist.video_urls:
     print(data, "\n")
     if data["cover_src"] != None:
         spaceless_album = data["album"].replace(' ', '')
-        spaceless_name = data["song"].replace(' ', '')
-        path = f'album_art/{spaceless_name}.jpg'
+        spaceless_name = data["title"].replace(' ', '').replace('(', '').replace(')', '')
+
         download_cover(data["cover_src"], spaceless_album)
         download_song(song_url, spaceless_name)
+
+        with taglib.File(f"content/songs/{spaceless_name}.mp3", save_on_exit=True) as mp3:
+            mp3.tags["TITLE"] = [data["title"]]
+            mp3.tags["ALBUM"] = [data["album"]]
+            mp3.tags["ARTIST"] = [data["artist"]]
