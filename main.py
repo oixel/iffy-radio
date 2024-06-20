e5ca719f53936c8a81ebb78922800ec17dfb87d1
oickle <oycell@aol.com>
2024-06-19 21:45:03 -0400

    Remove '/' from any album or song download names

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ playlist = Playlist(TEST_PLAYLIST_URL)
 for song_url in playlist.video_urls:
     dg = DG.DataGrabber(song_url)
     data = dg.get_data()
+
     print(data, "\n")
     if data["cover_src"] != None:
-        spaceless_album = data["album"].replace(' ', '')
-        spaceless_name = data["title"].replace(' ', '').replace('(', '').replace(')', '')
+        spaceless_album = data["album"].replace(' ', '').replace('/', '-')
+        spaceless_name = data["title"].replace(' ', '').replace('(', '').replace(')', '').replace('/', '-')
 
         download_cover(data["cover_src"], spaceless_album)
         download_song(song_url, spaceless_name)
@@ -22,3 +23,5 @@ for song_url in playlist.video_urls:
             mp3.tags["TITLE"] = [data["title"]]
             mp3.tags["ALBUM"] = [data["album"]]
             mp3.tags["ARTIST"] = [data["artist"]]
+        
+        print(data["title"], "downloaded and written!")
