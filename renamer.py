072ab64601f3fcf6b5d82afadb1c300bf649b1ae
oickle <oycell@aol.com>
2024-06-21 18:10:46 -0400

    Create fix ampersand function

--- renamer.py
+++ renamer.py
@@ -1,10 +1,16 @@
+def fix_ampersand(string) -> str:
+    if "u0026" in string:
+        string = string.replace("\\", "").replace("u0026", "&")
+    
+    return string
+
 def rename(name) -> str:
     new_name = ""
 
     ALLOWED_CHAR = "!?^&-,()[]*$@%#:;'\"+=_ "
 
-    if "u0026" in name:
-        name = name.replace("\\", "").replace("u0026", "&")
+    name = fix_ampersand(name)
+    
     for i in range(len(name)):
         c = name[i]
         if not c.isalnum() and c not in ALLOWED_CHAR:
