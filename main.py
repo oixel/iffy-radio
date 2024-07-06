ff38a03e90b84fc712a81b8a2578c666f7e72392
oickle <oycell@aol.com>
2024-07-06 14:25:31 -0400

    Add ESC as additional quit option

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ def main() -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    return
         
         screen.blit(background, (0, 0))
 
