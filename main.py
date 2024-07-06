45fdc616b9c8f2e5d3a7541620028068ce6be6e8
oickle <oycell@aol.com>
2024-07-06 14:23:43 -0400

    Add test GUI with pygame

--- main.py
+++ main.py
@@ -3,11 +3,31 @@ from downloader import *
 from renamer import *
 from pytube import Playlist
 from rfid_readerwriter import read_rfid
-import os
+import pygame
 
 def main() -> None:
-    data = read_rfid()
-    print(data)
+    # print("Please tap music card!")
+    # data = read_rfid()
+    # print(data)
+
+    pygame.init()
+    pygame.display.set_caption('iffy Radio')
+    screen = pygame.display.set_mode((800, 480))
+
+    background = pygame.Surface((800, 480))
+    background.fill(pygame.Color('#FFC0CB'))
+
+    is_running = True
+    
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+        
+        screen.blit(background, (0, 0))
+
+        pygame.display.update()
+    
 
 if __name__ == "__main__":
     main()
