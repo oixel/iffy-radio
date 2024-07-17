990c3f22ee065d6449cc986c89c395311c73b1fe
oickle <oycell@aol.com>
2024-07-17 13:44:18 -0400

    Implement pause button

--- ui_handler.py
+++ ui_handler.py
@@ -4,7 +4,7 @@ from gui_tools import *
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 480
 
 # Temporary function to be called when 1st test button is pressed
-def test1():
+def test1() -> None:
     background = pygame.Surface(SCREEN_SIZE)
     background.fill(pygame.Color('#d184a1'))
     screen.blit(background, (0, 0))
@@ -17,7 +17,7 @@ def test1():
     
 
 # Temporary function to be called when 2nd test button is pressed
-def test2():
+def test2() -> None:
     background = pygame.Surface(SCREEN_SIZE)
     background.fill(pygame.Color('#44752e'))
     screen.blit(background, (0, 0))
@@ -27,6 +27,13 @@ def test2():
     pygame.mixer.music.pause()
     pygame.mixer.music.load("content/songs/TEST/BBsV0Q7kGGY.mp3")
     pygame.mixer.music.play()
+
+# Toggles pause on music depending on its current state
+def toggle_pause() -> None:
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
+    else:
+        pygame.mixer.music.unpause()
 
 if __name__ == "__main__":
     # Creates a fullscreen window named "iffy radio"
@@ -44,13 +51,14 @@ if __name__ == "__main__":
     pressed_img_path = 'assets/sprites/test_button_pressed.png'
     test_button_1 = Button(screen, test1, (mid_x - 75, mid_y + 70), reg_img_path, pressed_img_path)
     test_button_2 = Button(screen, test2, (mid_x + 75, mid_y + 70), reg_img_path, pressed_img_path)
+    pause_button = Button(screen, toggle_pause, (mid_x, mid_y + 140), reg_img_path, pressed_img_path)
 
     song_info = SongInfo(screen, "content/songs/TEST/v1eypolupH0.mp3", (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 50))
     
     pygame.mixer.init()
     pygame.mixer.music.load("content/songs/TEST/v1eypolupH0.mp3")
     pygame.mixer.music.play()
-
+    
     is_running = True
     while is_running:
         for event in pygame.event.get():
@@ -62,6 +70,7 @@ if __name__ == "__main__":
 
         test_button_1.draw()
         test_button_2.draw()
+        pause_button.draw()
         song_info.draw()
 
         pygame.display.update()
