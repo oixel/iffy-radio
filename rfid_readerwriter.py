b3ec983a875019d007479792cc083fc1c1ddec16
oickle <oycell@aol.com>
2024-07-19 17:18:31 -0400

    Print read data

--- rfid_readerwriter.py
+++ rfid_readerwriter.py
@@ -20,12 +20,13 @@ def read_rfid() -> str:
 
     try:
         _, text = reader.read()
+        print(text)
     finally:
         GPIO.cleanup()
         return text
 
 if __name__ == "__main__":
-    choice = input("What do you desire? 1 = Write, 2 = Read")
+    choice = input("What do you desire? 1 = Write, 2 = Read\n")
     if int(choice) == 1:
         write_rfid()
         print("Data written!")
