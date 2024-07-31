09ae6360f54dc8482be3245854820612aeb8e000
oickle <oycell@aol.com>
2024-07-30 21:36:04 -0400

    Add comments to code and clean up a bit

--- main.py
+++ main.py
@@ -10,72 +10,90 @@ from downloader import *
 # Default dimensions of touchscreen
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 480
 
-#
+# Handles loading playlist and downloading non-downloaded songs
 def start() -> None:
-    # 
+    # Only utilizes RFID reading on Raspberry Pi
     if is_windows:
         playlist_url = "https://www.youtube.com/playlist?list=PL2fTbjKYTzKcb4w0rhNC76L-MER585BJa"
     else:
+        # Resets text back to requesting RFID card
         start_text.change_text("Tap playlist card!")
         render([background, start_text])
+
+        # Loads data in RFID into playlist URL when card is tapped
         playlist_url = f"https://www.youtube.com/playlist?list={read_rfid()}"
 
-    #
+    # Updates text to reflect current activity
     start_text.change_text("Verifying playlist...")
     render([background, start_text])
 
-    #
+    # Checks if playlist actually exists, if not, calls function again to request another card
     try:
         playlist = Playlist(playlist_url)
-        _ = playlist.video_urls[0]
+        _ = playlist.video_urls[0]  # Tests if playlist actually exists by checking it has at least one song
     except:
         start()
 
-    # 
+    # Updates text to reflect current activity
     start_text.change_text("Now checking for new songs..")
     render([background, start_text])
     
-    # 
+    # Creates list to track the URLs that need to be downloaded from
     not_downloaded = []
 
-    # 
+    # Loops through all downloaded songs and appends the URLs of non-downloaded songs to list
     for url in playlist.video_urls:
         file_name = url[32:]
 
+        # Adds paths of all songs to unalterable queue
         start_queue.append(f"songs/{file_name}.mp3")
 
+        # If song is not found in downloaded songs, append its URL to the list
         if not os.path.isfile(f"songs/{file_name}.mp3"):
             not_downloaded.append(url)
     
-    #
+    # Creates a non-linked copy of the unalterable queue to be able to be altered with shuffle
     global queue
     queue = start_queue.copy()
 
-    # 
+    # Only runs this functionality if there are undownloaded songs
     if len(not_downloaded) != 0:
+        # Keeps track of how many songs have been downloaded out of the missing songs  
         download_count = 0
+
+        # Updates text to display status of downloads
         start_text.change_text(f"{len(not_downloaded)} out of {len(playlist.video_urls)} songs not downloaded...")
         status_text.change_text(f"{download_count}/{len(not_downloaded)} downloaded!")
         render([background, start_text, status_text])
 
+        # Loops through all the missing songs
         for song_url in not_downloaded:
+            # Grabs the song's unique URL ID to set it as the song's filename
             file_name = song_url[32:]
 
+            # Renders error message if song failed to download
             if download_song(song_url, "", "songs/", file_name) == False:
                 status_text.change_text(f"ERROR SONG COULDN'T DOWNLOAD")
                 continue
             
+            # Writes any found song metadata to the downloaded MP3 
             dh = DataHandler(song_url)
             dh.write_data("", "songs/", file_name)
 
+            # Updates download status to reflect success
             download_count += 1
             status_text.change_text(f"{download_count}/{len(not_downloaded)} downloaded!")
-
             render([background, start_text, status_text])
 
+    # Updates state from start menu to player
     global state
     state = 1
 
+    # Changes rendered background color and updates the displayed metadata to be the first song in queue
+    background.change_color('#d184a1')
+    song_info.change_song(queue[track_num])
+
+    # Starts first song in queue!
     pygame.mixer.init()
     pygame.mixer.music.load(queue[track_num])
     pygame.mixer.music.play()
@@ -87,9 +105,9 @@ def render(to_render) -> None:
     
     pygame.display.update()
 
-#
+# Goes back to start menu from player menu
 def back() -> None:
-    global state, start_queue, queue, track_num, initial_state
+    global state, start_queue, queue, track_num
 
     # Resets all values of queue
     state = 0
@@ -97,24 +115,16 @@ def back() -> None:
     queue = []
     track_num = 0
 
-    #
-    initial_state = True
-
-    #
+    # Stops the music and closes the player
     pygame.mixer.music.stop()
     pygame.mixer.quit()
 
-    #
+    # Resets background and text on screen to their initial states
     background.change_color((0, 0, 0))
     start_text.change_text("Press Button to Start")
     render(start_ui)
 
-#
-def exit() -> None:
-    global is_running
-    is_running = False
-
-# 
+# Toggles the shuffle of the rest of the queue and ensures current song is left unaltered
 def shuffle() -> None:
     global start_queue, queue, track_num
 
@@ -128,9 +138,10 @@ def shuffle() -> None:
         # Places position in queue back to currently playing song's regular position in playlist
         track_num = start_queue.index(queue[track_num])
 
+        # Resets queue to its initial state when it was first loaded
         queue = start_queue.copy()
 
-#
+# Updates song info and loads new song into music player
 def load_song() -> None:
     song_info.change_song(queue[track_num])
 
@@ -140,10 +151,11 @@ def load_song() -> None:
     if paused:
         pygame.mixer.music.pause()
 
-# 
+# Resets song or plays previous song (or last song in queue if the queue's beginning has been reached)
 def previous() -> None:
     global track_num
-    
+
+    # Any elapsed time greater than this variable resets the current song rather than playing the previous song
     RESET_TIME = 4
 
     # Resets song to start unless button had been pressed in the last few seconds
@@ -152,7 +164,7 @@ def previous() -> None:
     
     load_song()
     
-# 
+# Skips to next song in queue (or first song if the queue's end has been reached)
 def skip() -> None:
     global track_num
     track_num = 0 if track_num == len(queue) - 1 else track_num + 1
@@ -177,10 +189,8 @@ if __name__ == "__main__":
     pygame.init()
     pygame.display.set_caption('iffy radio')
     
-    #
+    # Uses full screen and enables RFID checker when not using Windows
     is_windows = system() == "Windows"
-
-    #
     if is_windows:
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     else:
@@ -195,7 +205,7 @@ if __name__ == "__main__":
     # Tracks state to render proper UI elements
     state = 0
 
-    #
+    # Variables that are altered in functions
     start_queue = []  # Stores unshuffled queue
     queue = []  # Stores current queue (shuffled or not)
     track_num = 0
@@ -206,7 +216,7 @@ if __name__ == "__main__":
     reg_img_path = 'assets/textures/test_button.png'
     pressed_img_path = 'assets/textures/test_button_pressed.png'
 
-    #
+    # Creates a colored background that fills the entire screen
     background = Background(screen, (0, 0, 0))
 
     # UI Elements in start state
@@ -227,11 +237,10 @@ if __name__ == "__main__":
     back_button = Button(screen, back, (0, 0), reg_img_path, pressed_img_path)
     player_ui = [background, song_info, previous_button, skip_button, pause_button, shuffle_button, back_button]
 
-    #
-    initial_state = True
-
+    # Ensures loop runs from start
     is_running = True
     while is_running:
+        # Allows for quitting the radio by pressing the 'X' icon or tapping the 'ESC' key
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
@@ -239,23 +248,17 @@ if __name__ == "__main__":
                 if event.key == pygame.K_ESCAPE:
                     is_running = False
 
-        if state == 1 and initial_state:
-            # 
-            background.change_color('#d184a1')
-
-            # 
-            song_info.change_song(queue[0])
-
-            # 
-            initial_state = False
-
+        # Renders different UI elements depending on if in start menu (0) or player menu (1)
         if state == 0:
+            # Renders start menu UI
             render(start_ui)
         elif state == 1:
             # If the song is not paused and not playing anymore, then the song is over
             if not paused and not pygame.mixer.music.get_busy():
                 skip()
 
+            # Renders player UI
             render(player_ui)
     
+    # Closes down pygame when loop is quit
     pygame.quit()
