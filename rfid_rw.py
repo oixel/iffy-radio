6208240508c8ea81c7299d6a75d5ac29deffea71
oickle <oycell@aol.com>
2024-07-31 22:51:24 -0400

    Add copy data functionality

--- rfid_rw.py
+++ rfid_rw.py
@@ -28,10 +28,18 @@ def read_rfid() -> str:
         return text
 
 if __name__ == "__main__":
-    choice = input("What do you desire? 1 = Write, 2 = Read\n")
+    choice = input("What do you desire? 1 = Write, 2 = Read, 3 = Copy Data\n")
     if int(choice) == 1:
         write_rfid()
         print("Data written!")
     elif int(choice) == 2:
         print("Tap tag!")
         print(read_rfid())
+    elif int(choice) == 3:
+        print("Tap tag to copy data from!")
+        data = read_rfid()
+
+        print("Tap tag to paste data into!")
+        writer = MFRC()
+        writer.write(data)
+        
