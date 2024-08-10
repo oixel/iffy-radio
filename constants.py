1c2d5220723705b524cdde590055ed1f93791b69
oickle <oycell@aol.com>
2024-08-10 16:44:27 -0400

    Change default background to freight

--- constants.py
+++ constants.py
@@ -25,11 +25,9 @@ DEFAULT_TEST_URL = "https://www.youtube.com/playlist?list=PL2fTbjKYTzKcb4w0rhNC7
 # Any elapsed time greater than this variable resets the current song rather than playing the previous song
 RESET_TIME = 4
 
-# Constants relating to colored background
+# Constants relating to custom image backgrounds
 START_BG_PATH = "assets/backgrounds/test.png"
-DEFAULT_BG_PATH = "assets/backgrounds/road.png"
-START_BG_COLOR = (0, 0, 0)
-PLAYER_BG_COLOR = '#d184a1'
+DEFAULT_BG_PATH = "assets/backgrounds/freight.png"
 
 # Paths for test button UI
 REG_IMG_PATH = 'assets/textures/test_button.png'
@@ -40,14 +38,14 @@ BASIC_FONT_PATH = "assets/fonts/NotoSansRegular.ttf"
 SONG_INFO_FONT_PATH = "assets/fonts/NotoSansRegular.ttf"
 TIMER_FONT_PATH = "assets/fonts/ChivoMono.ttf"
 
-# Variables that stores text that doesn't get changed over time
+# Constants that stores text that doesn't get changed over time
 START_TEXT = "Press Button to Start"
 TAP_REQUEST_TEXT = "Tap playlist card!"
 VERIFYING_TEXT = "Verifying playlist..."
 CHECKING_TEXT = "Now checking for new songs.."
 ERROR_TEXT = "ERROR SONG COULDN'T DOWNLOAD"
 
-# Variables relating to font attributes
+# Constants relating to font attributes
 ARTIST_FONT_SIZE = 16
 ARTIST_FONT_COLOR = (0, 0, 0)
 SONG_FONT_SIZE = 24
@@ -55,13 +53,13 @@ SONG_FONT_COLOR = (0, 0, 0)
 TIME_FONT_SIZE = 16
 TIME_FONT_COLOR = (0, 0, 0)
 
-# Variables relating to the progress bar
+# Constants relating to the progress bar
 BAR_SIZE = (200, 10)
 CLICK_SAFETY = 20
 BAR_BG_COLOR = (0, 0, 0)
 BAR_PLAYING_COLOR = (255, 0, 0, 0)
 BAR_PAUSED_COLOR = (90, 90, 90)
 
-# Variables relating to the positioning of time stamps around the progress bar
+# Constants relating to the positioning of time stamps around the progress bar
 GAP = 25
 TIME_Y_OFFSET = 1
